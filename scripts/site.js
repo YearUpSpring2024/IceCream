76cc4f0d1d91b0f527c1d7ca99df70161700ba2a
Etson <etsond@ufl.edu>
2024-05-06 16:30:06 -0400

    updates

--- scripts/site.js
+++ scripts/site.js
@@ -3,57 +3,49 @@
 // radiobuttons
 const iceCreamConeRadio = document.getElementById("iceCreamConeRadio");
 const iceCreamCupRadio = document.getElementById("iceCreamCupRadio");
-// 
+// toppings
 const toppingsRow = document.getElementById("toppingsRow");
 const numberOfScoopsTextbox = document.getElementById("numberOfScoopsTextbox");
 const submitOrderButton = document.getElementById("submitOrderButton");
-
 // toppings
 const sprinklesCheckbox = document.getElementById("sprinklesCheckbox");
 const whippedCreamCheckbox = document.getElementById("whippedCreamCheckbox");
 const hotFudgeCheckbox = document.getElementById("hotFudgeCheckbox");
 const cherryCheckbox = document.getElementById("cherryCheckbox");
-
 // spans
 const outputTax = document.getElementById("outputTax");
 const outputTotal = document.getElementById("outputTotal");
 const outputBasePrice = document.getElementById("outputBasePrice");
-
 
 window.onload = function () {
     iceCreamConeRadio.onclick = togglesToppingRowVisibility;
     iceCreamCupRadio.onclick = togglesToppingRowVisibility;
     submitOrderButton.onclick = onSubmitOrderButtonClicked;
 }
-
-
 function togglesToppingRowVisibility() {
     console.log("toggle");
     let shouldBeVisible = iceCreamCupRadio.checked;
     // console.log(shouldBeVisible);
-
     if (shouldBeVisible) {
 
         toppingsRow.style.visibility = "visible";
     } else {
         toppingsRow.style.visibility = "hidden";
     }
-
 }
-
 function onSubmitOrderButtonClicked() {
     // prices
     const pricePerScoop = 2.25;
     const moreScoopAdded = 1.25;
     const taxRate = 0.07;
+
     let numberOfScoops = parseFloat(numberOfScoopsTextbox.value)
 
-    // staring at 0
+    // starting at 0
     let toppingsPrice = 0;
     let scoopPrice = 0;
     let tax = 0;
     let totalPrice = 0;
-
 
     if (sprinklesCheckbox.checked) {
         toppingsPrice = toppingsPrice + 0.50;
@@ -67,15 +59,11 @@ function onSubmitOrderButtonClicked() {
     if (cherryCheckbox.checked) {
         toppingsPrice = toppingsPrice + 0.25;
     }
-
     // Calculate scoop
     scoopPrice = pricePerScoop + ((numberOfScoops - 1) * moreScoopAdded);
-
     // calculate tax and total price
     tax = (scoopPrice + toppingsPrice) * taxRate;
     totalPrice = scoopPrice + toppingsPrice + tax;
-
-
     // showing the ouput
     outputBasePrice.innerHTML = `$${(scoopPrice + toppingsPrice).toFixed(2)}`;
     outputTax.innerHTML = `$${tax.toFixed(2)}`;
